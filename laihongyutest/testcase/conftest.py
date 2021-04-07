e4540317735fcb883952109729e51d3391820049
laihongyutest <742403861@qq.com>
2021-04-07 17:58:10 +0800

    测试alllure+添加conftest

--- laihongyutest/testcase/conftest.py
+++ laihongyutest/testcase/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 # conftest.py
 @pytest.fixture(scope='session')
-def get_userid():
+def get_token():
     token = 'qeehfjejwjwjej11sss@22'
     print('获取到token:%s' % token)
     return token
